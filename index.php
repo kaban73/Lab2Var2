efe9be275a6abbf13642932f279dd30fc7d28e68
Ivan Borkov <i.borkoff@yandex.ru>
2023-04-03 09:21:52 +0300

    begin events without scroll

--- index.php
+++ index.php
@@ -47,21 +47,100 @@
                 <div class="content_content">
                     <div class="row_content_content">
                         <div class="content_block_1">
-                            <div class="hello">
-                                <div class="row_hello">
-                                    <div class="image_hello">
-                                        <img src="images/hello.jpg" alt="">
+                            <div class="content_block_1_row">
+                                <div class="hello">
+                                    <div class="row_hello">
+                                        <div class="image_hello">
+                                            <img src="images/hello.jpg" alt="">
+                                        </div>
+                                        <div class="text_hello">
+                                            <div class="title_hello">
+                                                Hello!
+                                            </div>
+                                            <div class="subtitle_hello">
+                                                tationem laborum, impedit iusto consectetur, repudiandae pariatur fugiat, nostrum quis labore!
+                                            </div>
+                                            <div class="text_text_hello">
+                                                <p>pedit fugit amet asperiores aliquam est rerum. Voluptatibus porro beatae, eum perspiciatis aut provident odit natus ut. Inventore rerum dolor non nihil molestias illum ducimus, dolorum cum perferendis eveniet saepe, cupiditate aut atque magnam.</p>
+                                                <p>nderit tempore harum nulla aut quia? Aperiam, odio quos. Vero eligendi dolore similique in est mollitia quis expedita ex itaque reprehenderit deserunt aperiam sapiente, fuga sequi a provident dolor distinctio labore quibusdam aliquam.</p>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div class="text_hello">
-                                        <div class="title_hello">
-                                            Hello!
+                                </div>
+
+                                <div class="events">
+                                    <div class="row_events">
+                                        <div class="title_events">
+                                            Latest events
                                         </div>
-                                        <div class="subtitle_hello">
-                                            tationem laborum, impedit iusto consectetur, repudiandae pariatur fugiat, nostrum quis labore!
-                                        </div>
-                                        <div class="text_text_hello">
-                                            <p>pedit fugit amet asperiores aliquam est rerum. Voluptatibus porro beatae, eum perspiciatis aut provident odit natus ut. Inventore rerum dolor non nihil molestias illum ducimus, dolorum cum perferendis eveniet saepe, cupiditate aut atque magnam.</p>
-                                            <p>nderit tempore harum nulla aut quia? Aperiam, odio quos. Vero eligendi dolore similique in est mollitia quis expedita ex itaque reprehenderit deserunt aperiam sapiente, fuga sequi a provident dolor distinctio labore quibusdam aliquam.</p>
+                                        <div class="block_events">
+                                            <div class="scroll_events">
+                                                <div class="row_scroll_events">
+                                                    <div class="card_event">
+                                                        <div class="row_card_event">
+                                                            <div class="card_image">
+                                                                <div class="row_card_image">
+                                                                    <div class="card_image_image">
+                                                                        <img src="images/event_1.png" alt="">
+                                                                    </div>
+                                                                    <div class="card_image_date">
+                                                                        <div class="card_image_date_date">
+                                                                            <div class="date_num">24</div>
+                                                                            <div class="date_month">sept</div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="card_text">
+                                                                <div class="card_text_title">ddsadasdas</div>
+                                                                <div class="card_text_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque exercitationem doloremque iusto quo, commodi sint asperiores deleniti excepturi nulla eius.</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="card_event">
+                                                        <div class="row_card_event">
+                                                            <div class="card_image">
+                                                                <div class="row_card_image">
+                                                                    <div class="card_image_image">
+                                                                        <img src="images/event_1.png" alt="">
+                                                                    </div>
+                                                                    <div class="card_image_date">
+                                                                        <div class="card_image_date_date">
+                                                                            <div class="date_num">24</div>
+                                                                            <div class="date_month">sept</div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="card_text">
+                                                                <div class="card_text_title">ddsadasdas</div>
+                                                                <div class="card_text_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque exercitationem doloremque iusto quo, commodi sint asperiores deleniti excepturi nulla eius.</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="card_event">
+                                                        <div class="row_card_event">
+                                                            <div class="card_image">
+                                                                <div class="row_card_image">
+                                                                    <div class="card_image_image">
+                                                                        <img src="images/event_1.png" alt="">
+                                                                    </div>
+                                                                    <div class="card_image_date">
+                                                                        <div class="card_image_date_date">
+                                                                            <div class="date_num">24</div>
+                                                                            <div class="date_month">sept</div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                            <div class="card_text">
+                                                                <div class="card_text_title">ddsadasdas</div>
+                                                                <div class="card_text_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque exercitationem doloremque iusto quo, commodi sint asperiores deleniti excepturi nulla eius.</div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
